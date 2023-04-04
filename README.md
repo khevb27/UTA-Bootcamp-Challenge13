31b7453fe9906fb2521dbe1cacad4b4f9cbf4c3b
khevb27 <120353734+khevb27@users.noreply.github.com>
2023-04-04 02:33:58 -0400

    final updates and readme

--- README.md
+++ README.md
@@ -1,2 +1,85 @@
 # UTA-Bootcamp-Challenge13
-UTA-Bootcamp-Challenge13
+## Object-Relational Mapping (ORM) Challenge: E-commerce Back End
+----------------------------------------------------------------------
+## Description
+
+The purpose of this project is to improve the code by creating a backend using node.js, express.js server, and a mySQL database. It involves the creation of RESTful APIs that have CRUD functionality. Additionally, sequlize is used as an ORM tool to convert data to and from the database. Since this is a backend-only application, insomnia was used to make requests to the APIs and demonstrate their functionality.
+
+----------------------------------------------------------------------
+
+## User Story
+
+```
+AS A manager at an internet retail company
+I WANT a back end for my e-commerce website that uses the latest technologies
+SO THAT my company can compete with other e-commerce companies
+```
+
+## Acceptance Criteria
+
+```
+GIVEN a functional Express.js API
+WHEN I add my database name, MySQL username, and MySQL password to an environment variable file
+THEN I am able to connect to a database using Sequelize
+WHEN I enter schema and seed commands
+THEN a development database is created and is seeded with test data
+WHEN I enter the command to invoke the application
+THEN my server is started and the Sequelize models are synced to the MySQL database
+WHEN I open API GET routes in Insomnia Core for categories, products, or tags
+THEN the data for each of these routes is displayed in a formatted JSON
+WHEN I test API POST, PUT, and DELETE routes in Insomnia Core
+THEN I am able to successfully create, update, and delete data in my database
+```
+* "User Story" & "Acceptance Criteria" listed above have been provided by the UTA Bootcamp Program 2022.
+----------------------------------------------------------------------
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Built Using](#built-using)
+- [Preview](#preview)
+- [Links](#links)
+- [License](#license)
+
+----------------------------------------------------------------------
+
+## Installation
+
+1. Clone/download git repository.
+2. Run command "npm i"
+3. Run command "npm run seed".
+3. Run command "npm start" to run the application.
+
+#
+
+## Built Using
+
+- Node.js
+- Express.js
+- MySQL2
+- Sequelize
+- Insomnia
+
+#
+
+## Preview
+
+<img src= "assets/images/C13 Demo 1.gif"/>
+<img src= "assets/images/C13 Demo 2.gif"/>
+<img src= "assets/images/C13 Demo 3.gif"/>
+
+#
+
+## Links
+
+- Video Submission Link: https://drive.google.com/file/d/1S24DdMP94pc1OVIgQ6EkzJkER2q4byBq/view?usp=sharing 
+- GitHub Repository Link: https://github.com/khevb27/UTA-Bootcamp-Challenge13
+
+----------------------------------------------------------------------
+## License
+
+Please refer to licensing documentation in the project repository.
+
+<img src="https://img.shields.io/badge/license-MIT License-blue.svg" alt="GitHub License">
+
+----------------------------------------------------------------------
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,19 +4,41 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Tag, through: ProductTag, as: 'tags' }, {model: Category}]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      // JOIN with products, using the ProductTag through table
+      include: [{ model: Tag, through: ProductTag, as: 'tags' }, {model: Category}]
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -49,48 +71,73 @@ router.post('/', (req, res) => {
 
 // update product
 router.put('/:id', (req, res) => {
-  // update product data
+  // update product data 
   Product.update(req.body, {
     where: {
       id: req.params.id,
     },
   })
-    .then((product) => {
-      // find all associated tags from ProductTag
-      return ProductTag.findAll({ where: { product_id: req.params.id } });
-    })
-    .then((productTags) => {
-      // get list of current tag_ids
-      const productTagIds = productTags.map(({ tag_id }) => tag_id);
-      // create filtered list of new tag_ids
-      const newProductTags = req.body.tagIds
-        .filter((tag_id) => !productTagIds.includes(tag_id))
-        .map((tag_id) => {
-          return {
-            product_id: req.params.id,
-            tag_id,
-          };
+    .then(async () => {
+      // check if array of products tagIds were sent
+      if (req.body.tagIds && req.body.tagIds.length) {
+        // Find all of the tags for the product
+        const productTags = await ProductTag.findAll({
+          where: { product_id: req.params.id }
         });
-      // figure out which ones to remove
-      const productTagsToRemove = productTags
-        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
-        .map(({ id }) => id);
+        // transform the product tag data
+        const productTagIds = productTags.map(({ tag_id }) => tag_id);
 
-      // run both actions
-      return Promise.all([
-        ProductTag.destroy({ where: { id: productTagsToRemove } }),
-        ProductTag.bulkCreate(newProductTags),
-      ]);
+        /* When updating product tags, the array sent over may contain new tag_ids 
+           and it may also be void of tag_ids that used to be there.
+           We have to account for both situations:  */
+
+        // create filtered list of new tag_ids
+        const newProductTags = req.body.tagIds
+          // filter where the tag_ids coming over are not already in the current tag list
+          .filter((tag_id) => !productTagIds.includes(tag_id))
+          .map((tag_id) => {
+            return {
+              product_id: req.params.id,
+              tag_id,
+            };
+          });
+        // figure out which ones to remove
+        const productTagsToRemove = productTags
+          // filter where the current tag list has tags that are not present in the new tag list coming over
+          .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+          .map(({ id }) => id);
+        // run both actions asynchronously
+        Promise.all([
+          ProductTag.destroy({ where: { id: productTagsToRemove } }),
+          ProductTag.bulkCreate(newProductTags),
+        ]);
+        return res.json("Successfully updated with updated product tags")
+      }
+      return res.json("Successfully updated (no product tags included)");
     })
-    .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
       res.status(400).json(err);
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
